c17a79045b435bb363e9f60a7f381d812a3c812d
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-06-16 17:06:13 +0200

    change IHM

--- src/View/MyControlPanel.cpp
+++ src/View/MyControlPanel.cpp
@@ -15,22 +15,22 @@ MyControlPanel::MyControlPanel(wxWindow *parent) : wxPanel(parent)
 
 	m_parentFrame = (MyFrame *) parent ;
 
-	y = WIDGET_Y0 ;
-	m_button = new wxButton(this, ID_BUTTON1, wxT("Click me"), wxPoint(10, y)) ;
-	Bind(wxEVT_BUTTON, &MyControlPanel::OnButton, this, ID_BUTTON1) ;
+	// y = WIDGET_Y0 ;
+	// m_button = new wxButton(this, ID_BUTTON1, wxT("Click me"), wxPoint(10, y)) ;
+	// Bind(wxEVT_BUTTON, &MyControlPanel::OnButton, this, ID_BUTTON1) ;
 
-	y+= WIDGET_Y_STEP ;
-	wxStaticText* text1 = new wxStaticText(this, wxID_ANY, wxT("Radius"), wxPoint(10, y)) ;
+	// y+= WIDGET_Y_STEP ;
+	// wxStaticText* text1 = new wxStaticText(this, wxID_ANY, wxT("Radius"), wxPoint(10, y)) ;
 
-	y+= 15 ;
-	m_slider = new wxSlider(this, ID_SLIDER1, 10, 2, 100, wxPoint(10, y), wxSize(100,20), wxSL_LABELS) ;
-	Bind(wxEVT_SCROLL_THUMBTRACK, &MyControlPanel::OnSlider, this, ID_SLIDER1) ;
+	// y+= 15 ;
+	// m_slider = new wxSlider(this, ID_SLIDER1, 10, 2, 100, wxPoint(10, y), wxSize(100,20), wxSL_LABELS) ;
+	// Bind(wxEVT_SCROLL_THUMBTRACK, &MyControlPanel::OnSlider, this, ID_SLIDER1) ;
 
-	y+= WIDGET_Y_STEP ;
-	m_checkBox = new wxCheckBox(this, ID_CHECKBOX1, "Show (x,y)", wxPoint(10, y), wxSize(100,20)) ;
-	Bind(wxEVT_CHECKBOX, &MyControlPanel::OnCheckBox, this, ID_CHECKBOX1) ;
+	// y+= WIDGET_Y_STEP ;
+	// m_checkBox = new wxCheckBox(this, ID_CHECKBOX1, "Show (x,y)", wxPoint(10, y), wxSize(100,20)) ;
+	// Bind(wxEVT_CHECKBOX, &MyControlPanel::OnCheckBox, this, ID_CHECKBOX1) ;
 
-	y+= WIDGET_Y_STEP ;
+	y = WIDGET_Y0;
 	m_buttonRectangle = new wxButton(this, ID_BUTTON_RECT, wxT("Rectangle"), wxPoint(10, y)) ;
 	Bind(wxEVT_BUTTON, &MyControlPanel::OnButtonRectangle, this, ID_BUTTON_RECT) ;
 
@@ -42,21 +42,24 @@ MyControlPanel::MyControlPanel(wxWindow *parent) : wxPanel(parent)
 	m_buttonLine = new wxButton(this, ID_BUTTON_LINE, wxT("Ligne"), wxPoint(10, y)) ;
 	Bind(wxEVT_BUTTON, &MyControlPanel::OnButtonLine, this, ID_BUTTON_LINE) ;
 
-	y+= WIDGET_Y_STEP + 10 ;
+	y+= WIDGET_Y_STEP + 25;
+	wxStaticText* labelColorPickerOutline = new wxStaticText(this, wxID_ANY, wxT("Bordure"), wxPoint(10, y)) ;
+	y+= 20;
+	m_colourPickerOutline = new wxColourPickerCtrl(this, ID_COLOURPICKER_OUTLINE, wxColour(0,0,0,255), wxPoint(10, y), wxSize(40,40), 0, wxDefaultValidator, wxT("Couleur des contours")) ;
+	Bind(wxEVT_COLOURPICKER_CHANGED, &MyControlPanel::OnColorPickerOutline, this, ID_COLOURPICKER_OUTLINE) ;
+ 
+	y+= WIDGET_Y_STEP;
+	wxStaticText* labelColorPickerFill= new wxStaticText(this, wxID_ANY, wxT("Remplissage"), wxPoint(10, y)) ;
+	y+= 20;
+	m_colourPickerFill = new wxColourPickerCtrl(this, ID_COLOURPICKER_FILL, wxColour(0,0,0,255), wxPoint(10, y), wxSize(40,40), 0, wxDefaultValidator, wxT("Couleur de remplissage")) ;
+	Bind(wxEVT_COLOURPICKER_CHANGED, &MyControlPanel::OnColorPickerFill, this, ID_COLOURPICKER_FILL) ;
+
+	y+= WIDGET_Y_STEP + 50 ;
 	m_buttonLine = new wxButton(this, ID_BUTTON_SELECT, wxT("Sélection"), wxPoint(10, y)) ;
 	Bind(wxEVT_BUTTON, &MyControlPanel::OnButtonSelection, this, ID_BUTTON_SELECT) ;
-
-
-	y+= WIDGET_Y_STEP + 10 ;
-	m_colourPickerOutline = new wxColourPickerCtrl(this, ID_COLOURPICKER_OUTLINE, wxColour(0,0,0,255), wxPoint(10, y), wxDefaultSize, 0, wxDefaultValidator, wxT("Couleur des contours")) ;
-	Bind(wxEVT_COLOURPICKER_CHANGED, &MyControlPanel::OnColorPickerOutline, this, ID_COLOURPICKER_OUTLINE) ;
-
-
-	y+= WIDGET_Y_STEP + 10 ;
-	m_colourPickerFill = new wxColourPickerCtrl(this, ID_COLOURPICKER_FILL, wxColour(0,0,0,255), wxPoint(10, y), wxDefaultSize, 0, wxDefaultValidator, wxT("Couleur de remplissage")) ;
-	Bind(wxEVT_COLOURPICKER_CHANGED, &MyControlPanel::OnColorPickerFill, this, ID_COLOURPICKER_FILL) ;
 }
 
+/*
 //------------------------------------------------------------------------
 void MyControlPanel::OnButton(wxCommandEvent &event)
 {
@@ -76,7 +79,7 @@ void MyControlPanel::OnCheckBox(wxCommandEvent &event)
 	MyFrame* frame = (MyFrame*)GetParent() ;
 	frame->RefreshDrawing() ;	// update the drawing panel
 }
-
+*/
 //------------------------------------------------------------------------
 void MyControlPanel::OnButtonRectangle(wxCommandEvent &event)
 {
